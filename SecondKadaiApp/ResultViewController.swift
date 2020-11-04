a6226d63a92d4075113905464de09d85a52821be
ponte1010 <t.miwa.528@nitech.jp>
2020-11-04 18:09:50 +0900

    Auto Layout

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -12,20 +12,16 @@ class ResultViewController: UIViewController {
 
     // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
     @IBOutlet weak var textlabel: UILabel!
+    
     // 受け取るためのプロパティ（変数）を宣言しておく
-    var x:Int = 0
-    var y:Int = 0
+    var text:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
-        // 上記では、x, y を 0 と宣言していたが、
-        // 1画面目のViewControllerから遷移するときにprepareForSegueで
-        // x, yの値を新たに代入されたので両方共 1 が入っている
-        let result = x + y
-        textlabel.text = "結果は \(result) です"
+        let result = text
+        textlabel.text = "こんにちは \(result) さん"
     }
 
 }
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -14,13 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func button(_ sender: Any){
+    }
+    
+    @IBOutlet weak var textfield: UITextField!
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
-        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
-        resultViewController.x = 1
-        resultViewController.y = 1
+        
+        // 遷移先のResultViewControllerで宣言しているtextに値を代入して渡す
+        resultViewController.text = textfield.text!
     }
 
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
